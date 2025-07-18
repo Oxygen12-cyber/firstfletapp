6ac8adb4c3042cf8dbccf4afd963c7ea34c4b5a8
Oxygen12-cyber <adeolaodunjo32@gmail.com>
2025-07-18 16:13:39 -0400

    added IF functionality to TODO-PC version so as to prevent null tasks from registering

--- src/chatgptTodo_PC.py
+++ src/chatgptTodo_PC.py
@@ -2,14 +2,12 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title="CHATGPT ToDo PC APP"
+    page.title = "CHATGPT ToDo PC APP"
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.bgcolor="#FF2D2D30"
+    page.bgcolor = "#FF2D2D30"
     page.fonts = {"Kanit": "https://raw.githubusercontent.com/google/fonts/master/ofl/kanit/Kanit-Medium.ttf"}
-    page.theme=ft.Theme(font_family="Kanit")
-
-
+    page.theme = ft.Theme(font_family="Kanit")
 
     task_column = ft.Column(
         scroll=ft.ScrollMode.AUTO,
@@ -24,29 +22,30 @@ def main(page: ft.Page):
         cancel_button.update()
 
     def add_task(e):
-        check_box = ft.Checkbox(
-            label=box_input.value,
-            label_style=ft.TextStyle(color="white",size=24, weight=ft.FontWeight.NORMAL),
-            hover_color=ft.Colors.GREY_700,
-            active_color=ft.Colors.GREY_300,
-            check_color=ft.Colors.GREY_800,
-        )
-        task_column.controls.append(check_box)
-        box_input.value = ""
-        page.update()
+        if not box_input.value == "":
+            check_box = ft.Checkbox(
+                label=box_input.value,
+                label_style=ft.TextStyle(color="white", size=24, weight=ft.FontWeight.NORMAL),
+                hover_color=ft.Colors.GREY_700,
+                active_color=ft.Colors.GREY_300,
+                check_color=ft.Colors.GREY_800,
+            )
+            task_column.controls.append(check_box)
+            box_input.value = ""
+            page.update()
 
     def cancel_value(e):
         box_input.value = ""
         box_input.update()
 
     def onblur(e):
-        box_input.value=""
+        box_input.value = ""
         done_button.visible = False
         cancel_button.visible = False
         page.update()
 
-    done_button = ft.IconButton(icon=ft.Icons.DONE, visible=False, on_click=add_task)
-    cancel_button = ft.IconButton(icon=ft.Icons.CANCEL, visible=False, on_click=cancel_value)
+    done_button = ft.IconButton(icon=ft.Icons.DONE, visible=False, on_click=add_task, tooltip="save task", hover_color=ft.Colors.BLUE_GREY_300)
+    cancel_button = ft.IconButton(icon=ft.Icons.CANCEL, visible=False, on_click=cancel_value, tooltip="delete task", hover_color=ft.Colors.BLUE_GREY_300)
     box_input = ft.TextField(
         label="Add a task",
         on_focus=focus_mode,
@@ -103,14 +102,14 @@ def main(page: ft.Page):
                                 )
                             )
                         ],
-                        alignment=ft.MainAxisAlignment.CENTER),
+                            alignment=ft.MainAxisAlignment.CENTER),
                         margin=ft.margin.only(top=40),
                     ),
                     ft.Container(
                         ft.Row([
                             ft.Container(
                                 task_column,
-                                padding=ft.padding.only(top=20,left=20),
+                                padding=ft.padding.only(top=20, left=20),
                                 # width=380,
                                 expand=True,
                                 margin=ft.margin.only(left=20, right=20, top=40),
@@ -133,4 +132,6 @@ def main(page: ft.Page):
             )
         )
     )
+
+
 ft.app(main)
